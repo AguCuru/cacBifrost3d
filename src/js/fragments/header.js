8f8fd7ed12cf7dc3954c851b937161284549f887
GusCuru <elcurugrande@gmail.com>
2024-05-22 19:41:00 -0300

    hice de todo en V1

--- src/js/fragments/header.js
+++ src/js/fragments/header.js
@@ -1,19 +1,15 @@
     let header = 
     `
             <!-- Logo -->
-        <div id="menu">
-            <div id="logo">
-                <a href="./index.html">
-                    <span>Bifrost 3D</span>
-                </a>
-            </div>
+        
+            
             <nav class="navbar">
                 <ul>
-                    <li class=""><a href="./index.html">Inicio</a></li>
-                    <li class=""><a href="../pages/productos.html">Productos</a></li>
-                    <li class=""><a href="../pages/nosotros.html">Nosotros</a></li>
-                    <li class=""><a href="../pages/login.html">Login</a></li>
-                    <li class=""><a href="../pages/registro.html">Registro</a></li>
+                    <li class="navbar"><a href="./index.html">Inicio</a></li>
+                    <li class="navbar"><a href="../pages/productos.html">Productos</a></li>
+                    <li class="navbar"><a href="../pages/nosotros.html">Nosotros</a></li>
+                    <li class="navbar"><a href="../pages/login.html">Login</a></li>
+                    <li class="navbar"><a href="../pages/registro.html">Registro</a></li>
                 </ul>
             </nav>
         </div>    
