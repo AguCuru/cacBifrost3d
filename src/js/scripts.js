ebbe09b777f6daa36f49a5fc59f145f2755c6b76
Agustin Curutchet <agucuru@gmail.com>
2024-06-21 20:39:53 -0300

    [FEAT]responsive

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -203,3 +203,42 @@ AOS.init({
   offset: 1,
   duration: 1200,
 });
+
+// SECTION VIDEO
+
+/* script.js */
+
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".carousel-item");
+  if (index >= slides.length) {
+    currentIndex = 0;
+  } else if (index < 0) {
+    currentIndex = slides.length - 1;
+  } else {
+    currentIndex = index;
+  }
+
+  slides.forEach((slide, i) => {
+    if (i === currentIndex) {
+      slide.classList.add("active");
+    } else {
+      slide.classList.remove("active");
+    }
+  });
+
+  const carouselInner = document.querySelector(".carousel-inner");
+  carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+// Inicializar el carrusel
+showSlide(currentIndex);
